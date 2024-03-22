fa42e4f781c2db368786b5e650632514174f7458
Dallin Duke <nightshade0287@gmail.com>
2024-03-22 16:00:25 -0600

    Minor Changes to some assets

--- Assets/Scripts/PlayerScripts/PlayerUI.cs
+++ Assets/Scripts/PlayerScripts/PlayerUI.cs
@@ -10,13 +10,12 @@ public class PlayerUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpdateText(string.Empty);
     }
 
     // Update is called once per frame
     public void UpdateText(string promptMessage)
     {
-        Debug.Log("Text Updated");
+
         promptText.text = promptMessage;
     }
 }
